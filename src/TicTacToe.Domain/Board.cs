06e3066e3ebb4174580175c1ab66fd0050652c62
mindtnv <47498401+mindtnv@users.noreply.github.com>
2023-03-12 14:04:53 +0300

    Add Board and BoardTests

---
+++ src/TicTacToe.Domain/Board.cs
@@ -0,0 +1,84 @@
+﻿namespace TicTacToe.Domain;
+
+public class Board
+{
+    private readonly char[,] _board;
+    public readonly int Size;
+
+    public Board(int size) : this(size, new char[size, size])
+    {
+    }
+
+    public Board(int size, char[,] board)
+    {
+        Size = size;
+        _board = board;
+    }
+
+    public bool IsEmptyCell(int row, int col) => _board[row, col] == default(char);
+
+    public void PlaceSymbol(int row, int column, char symbol)
+    {
+        _board[row, column] = symbol;
+    }
+
+    public bool IsFull()
+    {
+        for (var i = 0; i < Size; i++)
+            for (var j = 0; j < Size; j++)
+                if (IsEmptyCell(i, j))
+                    return false;
+
+        return true;
+    }
+
+    public bool IsSymbolWon(char symbol)
+    {
+        // Check diagonals first
+        if (IsSymbolWonWithMainDiagonal(symbol) || IsSymbolWonWithSideDiagonal(symbol))
+            return true;
+
+        // Check all columns and rows
+        for (var i = 0; i < Size; i++)
+            if (IsSymbolWonWithColumn(symbol, i) || IsSymbolWonWithRow(symbol, i))
+                return true;
+
+        return false;
+    }
+
+    private bool IsSymbolWonWithColumn(char symbol, int column)
+    {
+        for (var i = 0; i < Size; i++)
+            if (_board[i, column] != symbol)
+                return false;
+
+        return true;
+    }
+
+    private bool IsSymbolWonWithRow(char symbol, int row)
+    {
+        for (var i = 0; i < Size; i++)
+            if (_board[row, i] != symbol)
+                return false;
+
+        return true;
+    }
+
+    private bool IsSymbolWonWithMainDiagonal(char symbol)
+    {
+        for (var i = 0; i < Size; i++)
+            if (_board[i, i] != symbol)
+                return false;
+
+        return true;
+    }
+
+    private bool IsSymbolWonWithSideDiagonal(char symbol)
+    {
+        for (var i = 0; i < Size; i++)
+            if (_board[i, Size - i - 1] != symbol)
+                return false;
+
+        return true;
+    }
+}
---
+++ src/TicTacToe.UnitTests/BoardTests.cs
@@ -0,0 +1,87 @@
+﻿using FluentAssertions;
+using TicTacToe.Domain;
+
+namespace TicTacToe.UnitTests;
+
+[TestFixture]
+public class BoardTests
+{
+    [Test]
+    public void IsEmptyCellTest()
+    {
+        var board = new Board(3);
+        board.PlaceSymbol(0, 0, 'T');
+        board.IsEmptyCell(0, 0).Should().BeFalse();
+        board.IsEmptyCell(1, 1).Should().BeTrue();
+    }
+
+    [Test]
+    public void IsFullTest()
+    {
+        var board = new Board(3);
+        for (var i = 0; i < board.Size; i++)
+            for (var j = 0; j < board.Size; j++)
+                board.PlaceSymbol(i, j, 'T');
+
+        board.IsFull().Should().BeTrue();
+    }
+
+    [Test]
+    public void IsNotFullTest()
+    {
+        var board = new Board(3);
+        for (var i = 0; i < board.Size - 1; i++)
+            for (var j = 0; j < board.Size - 1; j++)
+                board.PlaceSymbol(i, j, 'T');
+
+        board.IsFull().Should().BeFalse();
+    }
+
+    [Test]
+    public void SymbolWonWithColumnTest()
+    {
+        var board = new Board(3);
+        for (var i = 0; i < board.Size - 1; i++)
+            board.PlaceSymbol(i, 0, 'T');
+
+        board.IsSymbolWon('T').Should().BeFalse();
+        board.PlaceSymbol(board.Size - 1, 0, 'T');
+        board.IsSymbolWon('T').Should().BeTrue();
+    }
+
+    [Test]
+    public void SymbolWonWithMainDiagonalTest()
+    {
+        var board = new Board(3);
+        for (var i = 0; i < board.Size - 1; i++)
+            board.PlaceSymbol(i, i, 'T');
+
+        board.IsSymbolWon('T').Should().BeFalse();
+        board.PlaceSymbol(board.Size - 1, board.Size - 1, 'T');
+        board.IsSymbolWon('T').Should().BeTrue();
+    }
+
+    [Test]
+    public void SymbolWonWithRowTest()
+    {
+        var board = new Board(3);
+        for (var i = 0; i < board.Size - 1; i++)
+            board.PlaceSymbol(0, i, 'T');
+
+        board.IsSymbolWon('T').Should().BeFalse();
+        board.PlaceSymbol(0, board.Size - 1, 'T');
+        board.IsSymbolWon('T').Should().BeTrue();
+    }
+
+    [Test]
+    public void SymbolWonWithSideDiagonalTest()
+    {
+        var board = new Board(3);
+        for (var i = 0; i < board.Size - 1; i++)
+            board.PlaceSymbol(i, board.Size - i - 1, 'T');
+
+        board.IsSymbolWon('T').Should().BeFalse();
+        board.PlaceSymbol(board.Size - 1, 0, 'T');
+        board.IsSymbolWon('T').Should().BeTrue();
+    }
+}
